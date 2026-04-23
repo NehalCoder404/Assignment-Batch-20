8956d5e3d89e2baa066331f92f88c594b16113a7
Nehal Ali <nehalalikamran103@gmail.com>
2026-04-23 01:57:02 -0700

    revision

---
+++ Revision 21-40/app.js
@@ -0,0 +1,31 @@
+
+console.log('js runing');
+
+let paragraph = 'my nehal name is nehal'
+
+
+// console.log(paragraph.charAt(0))
+console.log(paragraph[4])
+
+// console.log(para.slice(0,4));
+
+
+    // console.log(paragraph.replace(/nehal/g, "ahsan"))
+
+
+
+    
+
+let val = 45.45
+// console.log(Math.round(val))    // 5 >= 6   // 5 < 4
+// console.log(Math.random())
+
+// console.log(typeof val.toFixed())
+// console.log( val.toFixed())
+
+
+let n1 = "45.45"
+console.log(typeof n1)
+// console.log(parseFloat(n1))
+console.log(Number(n1))
+console.log(+n1)
